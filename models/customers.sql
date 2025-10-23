6952fd0da170284ca8517dc6ecae44018d520f73
Espen Haugen <espenhaugen14@gmail.com>
2025-10-23 04:59:40 +0000

    oppdatert med masse god kode

---
+++ models/customers.sql
@@ -0,0 +1,42 @@
+
+{{config(materialized="view")}}
+with customers as (
+    select 
+     customer_id,
+     first_name,
+     last_name
+     from {{ref("stg_customers")}}
+),
+
+orders as
+ (
+    select 
+    order_id,
+    customer_id,
+    order_date
+    from   {{ref("stg_orders")}}
+
+ ),
+ customer_orders as
+ (
+    select 
+    customer_id,
+    min(order_date) as first_order_date,
+    max(order_date) as most_recent_order_date,
+    count(order_id) as number_of_orders
+    from orders 
+    group by customer_id
+ ),
+ final as
+ (
+    select 
+    customers.customer_id,
+    customers.first_name,
+    customers.last_name,
+    customer_orders.first_order_date,
+    customer_orders.most_recent_order_date,
+    customer_orders.number_of_orders
+    from customers 
+     inner join customer_orders on customers.customer_id=customer_orders.customer_id
+ )
+ select * from final
